391914e3589c70888adb1670501174717f8e3b6c
saidamir <skadirov2009@gmail.com>
2024-02-06 16:25:22 +0500

    js

---
+++ main.js
@@ -0,0 +1,31 @@
+// data-types:
+// 1.integeer(numbers)- 1,2,3,4;
+// 2.string(worlds)- 'hello','hi';
+// 3.boolen- true/false;
+// 4.bigintegeer- n(year,huge numbers);
+// 5.NaN- (Not a number);
+// 6.nul- ()
+
+// const , let - Переменная;
+// [] - Массив
+// {} - object 
+// () - function
+
+/*
+let nember = 'hygyhyhyhg';
+const nuberConst = 0;
+console.log(typeof(nember));
+console.log(nuberConst);
+console.log(nember);
+nember = 229292823983903939;
+let togri = true
+console.log(nember);
+console.log(typeof(togri));
+let big = 200135453434432434343784264278462472523674547625426745276n;
+console.log(typeof(big));
+*/
+
+alert ( 'hello world' )
+
+let number = 12;
+console.log(typeof(number));
